09160497b6c1f31289bed248671b15c06cd9c55a
Snoopy81 <angelos81@gmail.com>
2024-01-16 10:01:31 +0900

    Add files via upload

---
+++ src/main/java/com/dhtmlx/DhtmlxApplication.java
@@ -0,0 +1,13 @@
+package com.dhtmlx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhtmlxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhtmlxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhtmlx/DhtmlxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhtmlx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhtmlxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
